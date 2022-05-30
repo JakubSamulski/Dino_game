dcec45ad37a2e58c4e19bf7d6fdab4dd5bf68b6b
DogeXD <44587062+DogeXD@users.noreply.github.com>
2022-05-30 14:38:34 +0200

    added jumping,ducking and background

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 import os
-pygame.init()
 
+import time
+import random
 #Constants
 
+
+pygame.init()
 SCREEN_H=729
 SCREEN_W=1280
 
@@ -31,9 +34,31 @@ CLOUD = pygame.image.load(r'Assets/Other/Cloud.png')
 BACKGROUND = pygame.image.load(r'Assets/Other/Track.png')
 
 
+class Cloud:
+    def __init__(self):
+        self.x_position = SCREEN_W +random.randint(800,1000)
+        self.y_position = random.randint(50,100)
+        self.image = CLOUD
+        self.width = self.image.get_width()
+
+    def draw(self,SCREEN):
+        SCREEN.blit(self.image,(self.x_position,self.y_position))
+
+    def update(self):
+        self.x_position -= game_speed
+        if self.x_position < -self.width:
+            self.x_position = SCREEN_W + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
+
+
+
+
 class Dinosaur:
     x_position=80
     y_position = 310
+    y_position_duck=340
+    JUMP_V = 8.5
+
     def __init__(self):
         self.duck_images = DINO_DUCKING
         self.run_images = DINO_RUNNING
@@ -43,7 +68,7 @@ class Dinosaur:
         self.is_ducking = False
         self.is_running = True
         self.is_jumping = False
-
+        self.jump_velocity = self.JUMP_V
         self.step_index =0
         self.image = self.run_images[0]
 
@@ -96,17 +121,51 @@ class Dinosaur:
         self.step_index +=1
 
     def jump(self):
-        pass
+        self.image = self.jump_images
+        if self.is_jumping:
+            self.dino_rect.y-=self.jump_velocity*4
+            self.jump_velocity -=0.8
+        #if self.dino_rect.y>=self.y_position:
+        if self.jump_velocity< - self.JUMP_V:
+            self.is_jumping=False
+            self.jump_velocity = self.JUMP_V
+
     def duck(self):
-        pass
+        self.image = self.duck_images[self.step_index//5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_position
+        self.dino_rect.y = self.y_position_duck
+        self.step_index += 1
+
     def draw(self,SCREEN):
         ''' draws dino to the screen'''
         SCREEN.blit(self.image,(self.dino_rect.x,self.dino_rect.y))
 
+
 def main():
+    global game_speed,x_position_background,y_position_background
+    game_speed=14
+    x_position_background=0
+    y_position_background=380
+
+    def background():
+        global y_position_background,x_position_background
+        image_width = BACKGROUND.get_width()
+        SCREEN.blit(BACKGROUND,(x_position_background,y_position_background))
+        SCREEN.blit(BACKGROUND, (image_width+x_position_background, y_position_background))
+        if(x_position_background < -image_width):
+            SCREEN.blit(BACKGROUND, (image_width + x_position_background, y_position_background))
+            x_position_background=0
+        x_position_background -= game_speed
+
+
+
+
     run = True
     clock  = pygame.time.Clock()
     dino = Dinosaur()
+    cloud=  Cloud()
+
 
     while run:
         for event in pygame.event.get():
@@ -114,9 +173,11 @@ def main():
                 run = False
         SCREEN.fill((255,255,255))
         user_input = pygame.key.get_pressed()
-
+        background()
         dino.draw(SCREEN)
         dino.update(user_input)
+        cloud.draw(SCREEN)
+        cloud.update()
         clock.tick(30)
         pygame.display.update()
 
