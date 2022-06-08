ad7f89124f5eae003e4b80e2c1a1b6cfc15fd83f
DogeXD <44587062+DogeXD@users.noreply.github.com>
2022-06-09 00:35:09 +0200

    refactor of button and textbox

--- Button.py
+++ Button.py
@@ -1,44 +1,97 @@
 import pygame
 
+
 class Button:
-    def __init__(self,SCREEN,left=0,top=0,width=50,height=20,text=''):
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(self.left,self.top,self.width,self.height)
-        self.SCREEN = SCREEN
-        self.border_color = (0,0,0)
+    def __init__(self, SCREEN: pygame.display, left: int = 0, top: int = 0, width: int = 50, height: int = 20,
+                 text: str = '') -> None:
+        self.__left = left
+        self.__top = top
+        self.__width = width
+        self.__height = height
+        self.__rect = pygame.Rect(self.__left, self.__top, self.__width, self.__height)
+        self.__SCREEN = SCREEN
+        self.__border_color = (0, 0, 0)
 
-        self.text = text
-        self.font_size = 25
-        self.font_object = pygame.font.Font('freesansbold.ttf', self.font_size)
-        self.font_color = (0,0,0)
+        self.__text = text
+        self.__font_size = 25
+        self.__font_object = pygame.font.Font('freesansbold.ttf', self.__font_size)
+        self.__font_color = (0, 0, 0)
 
+    def set_pos(self, left: int, top: int, width: int, height: int) -> None:
+        if left < 0 or top < 0 or width < 0 or height < 0:
+            raise ValueError("negative coordinates")
 
-    def draw_button(self):
-        pygame.draw.rect(self.SCREEN, self.border_color, self.rect, 1)
+        self.__left = left
+        self.__top = top
+        self.__width = width
+        self.__height = height
 
-    def draw_text(self):
-        text = self.font_object.render(self.text, True, self.font_color)
+    def get_pos(self):
+        return self.__left, self.__top, self.__width, self.__height
+
+    def set_rect(self, rect: pygame.rect):
+        self.__rect = rect
+
+    def get_rect(self):
+        return self.__rect
+
+    def set_screen(self, screen: pygame.display):
+        self.__SCREEN = screen
+
+    def get_screen(self):
+        return self.__SCREEN
+
+    def set_border_color(self, color: tuple[int, int, int]):
+        for i in range(3):
+            if color[i] < 0 or color[i] > 255:
+                raise ValueError("Color must be between 0 adn 255")
+        self.__border_color = color
+
+    def get_border_color(self):
+        return self.__border_color
+
+    def get_text(self):
+        return self.__text
+
+    def set_text(self, text: str):
+        self.__text = text
+
+    def set_font_size(self, size: int):
+        if size < 1:
+            raise ValueError("Font size must be >0")
+        self.__font_size = size
+
+    def get_font_size(self):
+        return self.__font_size
+
+    def set_font(self, font_obj: pygame.font.Font):
+        self.__font_object = font_obj
+
+    def get_font(self):
+        return self.__font_object
+
+    def set_font_color(self, color: tuple[int, int, int]):
+        for i in range(3):
+            if color[i] < 0 or color[i] > 255:
+                raise ValueError("Color must be between 0 adn 255")
+        self.__font_color = color
+
+    def draw_button(self) -> None:
+        pygame.draw.rect(self.__SCREEN, self.__border_color, self.__rect, 1)
+
+    def draw_text(self) -> None:
+        text = self.__font_object.render(self.__text, True, self.__font_color)
         text_rect = text.get_rect()
-        text_rect.center = self.rect.center
-        self.SCREEN.blit(text, text_rect)
+        text_rect.center = self.__rect.center
+        self.__SCREEN.blit(text, text_rect)
 
-    def draw(self):
+    def draw(self) -> None:
         self.draw_button()
         self.draw_text()
-    def update(self):
+
+    def update(self) -> None:
         self.draw()
 
-    def is_clicked(self):
+    def is_clicked(self) -> bool:
         mouse_pos = pygame.mouse.get_pos()
-        return self.rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
-
-
-
-
-
-
-
-
+        return self.__rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
--- CustomTextbox.py
+++ CustomTextbox.py
@@ -1,60 +1,75 @@
 import pygame_textinput
 import pygame
 
+
 # TODO napisac to ładniej i zrobic walidacje
 class Textbox(pygame_textinput.TextInputVisualizer):
-    def __init__(self, left=0, top=0, width=10, height=25, default_text=''):
+    def __init__(self, left:int=0, top:int=0, width:int=10, height:int=25, default_text:str='')->None:
 
         super().__init__()
+        self.__left = left
+        self.__top = top
+        self.__width = width
+        self.__height = height
+        self.__is_active = False
+        self.__default_text = default_text
+        self.__has_been_clicked = -1
+        self.__border_color = (0, 0, 0)
+        self.__font_object = pygame.font.Font('freesansbold.ttf', 25)
+        self.__rect = super(Textbox, self).surface.get_rect()
+        self.__rect.left = self.__left - 5
+        self.__rect.top = self.__top - 3
+        self.__rect.width = self.__width
+        self.__rect.height = self.__height + 4
 
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
-        self.is_active = False
-        self.default_text = default_text
-        self.has_been_clicked = -1
-        self.initialize()
-        self.border_color = (0, 0, 0)
-        self.font_object = pygame.font.Font('freesansbold.ttf', 25)
-
-    def initialize(self):
-        self.rect = super(Textbox, self).surface.get_rect()
-        self.rect.left = self.left - 5
-        self.rect.top = self.top -3
-        self.rect.width = self.width
-        self.rect.height = self.height + 4
-
-        if self.default_text != '':
+        if self.__default_text != '':
             self.set_default_text()
 
-    def setSize(self, left=0, top=0, width=10, height=25):
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
 
-    def set_font_color(self, color: tuple[3]):
+    def set_size(self, left:int=0, top:int=0, width:int=10, height:int=25)->None:
+        self.__left = left
+        self.__top = top
+        self.__width = width
+        self.__height = height
+    def get_size(self):
+        return self.__left,self.__top,self.__width,self.__height
+
+    def set_font_color(self, color: tuple[int, int, int]):
+        for i in range(3):
+            if color[i] < 0 or color[i] > 255:
+                raise ValueError("Color must be between 0 adn 255")
         self.font_color = color
 
-    def set_default_text(self):
-        self.value = self.default_text
+    def get_font_color(self):
+        return self.font_color
+
+    def set_default_text(self)->None:
+        self.value = self.__default_text
         self.set_font_color((170, 170, 170))
 
-    def set_border_color(self, color: tuple[int, int, int]):
-        self.border_color = color
+    def set_font(self, font_obj: pygame.font.Font):
+        self.__font_object = font_obj
 
-    def get_text(self):
+    def get_font(self):
+        return self.__font_object
+    def set_rect(self, rect: pygame.rect):
+        self.__rect = rect
+
+    def get_rect(self):
+        return self.__rect
+
+    def set_border_color(self, color: tuple[int, int, int])->None:
+        for i in range(3):
+            if color[i] < 0 or color[i] > 255:
+                raise ValueError("Color must be between 0 adn 255")
+        self.__border_color = color
+
+    def get_text(self)->str:
         return self.value
+    def set_text(self,text:str):
+        self.value =text
 
-
-
-
-
-
-
-
-    def update(self, events: pygame.event.Event, SCREEN):
+    def update(self, events: pygame.event.Event, SCREEN:pygame.display)->None:
         '''
         function called every frame
         :param events: result of pygane.event.get()
@@ -64,13 +79,13 @@ class Textbox(pygame_textinput.TextInputVisualizer):
 
         # print(self.font_color)
         def draw():
-            if (self.rect.width < self.surface.get_rect().width):
-                self.rect.width = self.surface.get_rect().width
-            elif self.rect.width > self.surface.get_rect().width and self.surface.get_rect().width > self.width:
-                self.rect.width = self.surface.get_rect().width
-            pygame.draw.rect(SCREEN, self.border_color, self.rect, 2)
+            if (self.__rect.width < self.surface.get_rect().width):
+                self.__rect.width = self.surface.get_rect().width
+            elif self.__rect.width > self.surface.get_rect().width and self.surface.get_rect().width > self.__width:
+                self.__rect.width = self.surface.get_rect().width
+            pygame.draw.rect(SCREEN, self.__border_color, self.__rect, 2)
             # print(self.surface.get_rect().width)
-            SCREEN.blit(self.surface, (self.left, self.top))
+            SCREEN.blit(self.surface, (self.__left, self.__top))
 
         # SCREEN.blit(self.surface, (200, 200))
         mousePos = pygame.mouse.get_pos()
@@ -78,17 +93,17 @@ class Textbox(pygame_textinput.TextInputVisualizer):
         # pygame.draw.rect(SCREEN, (255, 0, 0), self.rect, 2)
         # print(self.rect)
         # checks if textbox should be active or not and calls update if it is active
-        if self.has_been_clicked == 0:
+        if self.__has_been_clicked == 0:
             self.value = ''
-            self.has_been_clicked = 1
+            self.__has_been_clicked = 1
 
-        if self.rect.collidepoint(mousePos) and pygame.mouse.get_pressed()[0] and not self.is_active:
-            self.has_been_clicked = 0
-            self.is_active = True
+        if self.__rect.collidepoint(mousePos) and pygame.mouse.get_pressed()[0] and not self.__is_active:
+            self.__has_been_clicked = 0
+            self.__is_active = True
             self.font_color = (0, 0, 0)
-        elif not self.rect.collidepoint(mousePos) and pygame.mouse.get_pressed()[0] and self.is_active:
-            self.is_active = False
+        elif not self.__rect.collidepoint(mousePos) and pygame.mouse.get_pressed()[0] and self.__is_active:
+            self.__is_active = False
             self.cursor_visible = False
-        if self.is_active:
+        if self.__is_active:
             super().update(events)
         draw()
